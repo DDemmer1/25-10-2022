e12e0a9317791ce245f1d9847c4ff64c9dba8772
Unknown <Unknown@DESKTOP-4KSK0J5.fritz.box>
2022-10-25 11:17:39 +0200

    initial commit

--- src/de/demmer/dennis/Application.java
+++ src/de/demmer/dennis/Application.java
@@ -1,6 +1,44 @@
 package de.demmer.dennis;
 
 public class Application {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello World");
+		
+		addieren(5, 8);
+
+		int ergebnisMulti = multiply(2, 5);
+
+		System.out.println(ergebnisMulti+ " (aus der main-methode)");
+	}
+	
+	
+
+	static void addieren(int zahl1, int zahl2) {
+
+		// STRG * 7 => zeile als Kommentar
+		// System.out.println(zahl1+zahl2);
+
+		int ergebnis = zahl1 + zahl2;
+
+		// sysout + STRG + LEER = System.out.println()
+		System.out.println(ergebnis);
+	}
+	
+	
+	
+	static int multiply(int zahl1, int zahl2) {
+			
+//		return zahl1*zahl2;
+		int ergebnis = zahl1*zahl2;
+		System.out.println(ergebnis + " (aus der Methode mit return)");
+		
+		return ergebnis;
+	}
+	
+	
+	
 	
 
 }
